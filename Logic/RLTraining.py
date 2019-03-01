15c78b343be922bc21317f3f4cdea92b90e959fb
Sara Ghaemi <ghaemi.sr@gmail.com>
2019-03-01 16:47:01 -0700

    Tried to do RL, it was too complicated!
    Co-Authored-By: Sara Gholami <srghlm6@gmail.com>

--- Logic/RLTraining.py
+++ Logic/RLTraining.py
@@ -1,22 +1,30 @@
 import numpy as np
 
+class Train:
+    def __init__(self, board):
+        self.board = board
+        self.board_values = np.array([[4, -3, 2, 2, 2, 2, -3, 4],
+                                 [-3, -4, -1, -1, -1, -1, -4, -3],
+                                 [2, -1, 1, 0, 0, 1, -1, 2],
+                                 [2, -1, 0, 1, 1, 0, -1, 2],
+                                 [2, -1, 0, 1, 1, 0, -1, 2],
+                                 [2, -1, 1, 0, 0, 1, -1, 2],
+                                 [-3, -4, -1, -1, -1, -1, -4, -3],
+                                 [4, -3, 2, 2, 2, 2, -3, 4]])
+        self.board_size = board.shape[0]
+        self.value = {}
 
-def convert_board_to_index(board):
-    index = 0
-    for i in range(board.shape[0]):
-        for j in range(board.shape[0]):
-            index *= 3
-            index += board[i][j]
-    return int(index)
+    def convert_board_to_index(self, board):
+        index = 0
+        for i in range(board.shape[0]):
+            for j in range(board.shape[0]):
+                index *= 3
+                index += board[i][j]
+        return int(index)
 
-
-board_size = 3
-value = {}
-test = np.zeros((board_size, board_size))
-test[0][0] = 2
-result = convert_board_to_index(test)
-print(result)
-if result not in value:
-    value[result] = 10
-    print(value)
-
+    def calc_value(self, board):
+        result = self.convert_board_to_index(board)
+        print(result)
+        if result not in self.value.keys():
+            self.value[result] = 10
+            print(self.value)
